bcf661a46bcb089e231013a1792461f71729c23f
VinVel05 <git@velcore.net>
2026-07-15 02:08:01 +0200

    Update LICENSE

--- index.ts
+++ index.ts
@@ -1,16 +1,11 @@
 /*
  * Copyright (c) 2026 VinVel
  *
- * SPDX-License-Identifier: AGPL-3.0-only
+ * SPDX-License-Identifier: MPL-2.0
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, version 3 only.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- *
- * Project home: velcore.net
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
 export * from "./context";
